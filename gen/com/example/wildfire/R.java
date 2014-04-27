e026806e200b1db357c4015cd6815580e0cef97d
madthad91 <thaddeus.madison@gmail.com>
2014-04-27 12:51:10 -0700

    added a blank wifi manager class and a dummy data access object to the model folder. Created model and controller folders.

--- gen/com/example/wildfire/R.java
+++ gen/com/example/wildfire/R.java
@@ -30,15 +30,19 @@ public final class R {
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
-        public static final int fragment_main=0x7f030001;
+        public static final int activity_wifi_manager=0x7f030001;
+        public static final int fragment_main=0x7f030002;
+        public static final int fragment_wifi_manager=0x7f030003;
     }
     public static final class menu {
         public static final int main=0x7f070000;
+        public static final int wifi_manager=0x7f070001;
     }
     public static final class string {
         public static final int action_settings=0x7f050002;
         public static final int app_name=0x7f050000;
         public static final int hello_world=0x7f050001;
+        public static final int title_activity_wifi_manager=0x7f050003;
     }
     public static final class style {
         /** 
---
+++ src/controllers/WifiManager.java
@@ -0,0 +1,69 @@
+package controllers;
+
+import com.example.wildfire.R;
+import com.example.wildfire.R.id;
+import com.example.wildfire.R.layout;
+import com.example.wildfire.R.menu;
+
+import android.app.Activity;
+import android.app.ActionBar;
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.os.Build;
+
+public class WifiManager extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_wifi_manager);
+
+		if (savedInstanceState == null) {
+			getFragmentManager().beginTransaction()
+					.add(R.id.container, new PlaceholderFragment()).commit();
+		}
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.wifi_manager, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+
+	/**
+	 * A placeholder fragment containing a simple view.
+	 */
+	public static class PlaceholderFragment extends Fragment {
+
+		public PlaceholderFragment() {
+		}
+
+		@Override
+		public View onCreateView(LayoutInflater inflater, ViewGroup container,
+				Bundle savedInstanceState) {
+			View rootView = inflater.inflate(R.layout.fragment_wifi_manager,
+					container, false);
+			return rootView;
+		}
+	}
+
+}
